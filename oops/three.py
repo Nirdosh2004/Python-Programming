2b96a1a0b73f606c5c007916254ea2b245491b39
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-06-30 18:54:49 +0530

    done

--- oops/three.py
+++ oops/three.py
@@ -1,9 +1,30 @@
 class student:
-          name = "nirdosh kushwaha"
-          def __init__(self):
-                  print(self)
-                  print("Hello brother")
-          
+          def __init__(self , name , marks):
+                  self.name = name
+                  self.marks = marks
 
-s1 = student()
-# print(s1)
+
+s1 = student("rishabh" , 74)
+print(s1.name)
+
+s2 = student("Nirdosh" , 99)
+print(s2.marks)
+print(s2.name)
+
+
+
+# class
+
+class school:
+          def __init__(self , name , marks):
+                  self.name = name
+                  self.marks = marks
+
+
+s1 = school("Nirdosh kushwaha" , 90)
+print(s1.name )
+print(s1.marks)
+
+s2 = school("Aishwary Mishra" , 99)
+print(s2.marks)
+print(s2.name)
